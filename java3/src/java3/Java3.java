22465b2a12a8857a84e1c4c420cf1409701e3ff9
Levi Bergamascki <82347503+levibergamascki@users.noreply.github.com>
2022-03-18 12:14:33 -0300

    Add files via upload

---
+++ java3/src/java3/Java3.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java3;
+
+/**
+ *
+ * @author 2etimds
+ */
+public class Java3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+         System.out.print("Digite um número inteiro: ");
+         
+         double num; 
+         
+         num = ler.nextDouble; 
+           
+         String[] unidade = {" ", "um", "dois", "três", "quatro", "cinco", "seis","sete", "oito", "nove"};
+         
+         String[] dezena = {"", "vinte", "trinta", "quarenta", "cinquenta", "sessenta", "setenta", "oitenta", "noventa"}; 
+         
+         String[] centena = {"", "duzentos", "trezentos", "quatrocentos", "quinhentos", "seiscentos", "setencentos", "oitocentos", "novecentos"}; 
+         
+    }
+        
+    }
+  
