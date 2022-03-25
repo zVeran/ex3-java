cde419131f838e6671693ff6ffeeba91de2202c2
Luiz Gustavo <81664581+zVeran@users.noreply.github.com>
2022-03-25 10:39:31 -0300

    Update Java3.java

--- java3/src/java3/Java3.java
+++ java3/src/java3/Java3.java
@@ -20,23 +20,21 @@ public class Java3 {
         um();
     }
     public static void extenso(String a, int b){
-		// O valor do parametro 'a' é a coluna, o 'b' é a linha  ( eu posso ter invertido linha com coluna )
-		// por isso, se 'a' for '5' e 'b' for '2' o valor retornado será "cinquenta"
+		
         String[][] unidade = {
-            {"dez","zero", "", "", ""}, // 0
-            {"onze","um", "", "cento", "mil"}, // 1
-            {"doze", "dois", "vinte", "duzentos", "dois mil"}, // 2
-            {"treze", "três", "trinta", "trezentos", "três mil"}, // 3
-            {"quatorze", "quatro", "quarenta", "quatrocentos", "quatro mil"}, // 4
-            {"quinze", "cinco", "cinquenta", "quinhentos", "cinco mil"}, // 5
-            {"dezesseis", "seis", "sessenta", "seiscentos", "seis mil"}, // 6
-            {"dezesete", "sete", "setenta", "setecentos", "sete mil"}, // 7
-            {"dezoito", "oito", "oitenta", "oitocentos", "oito mil"}, // 8
-            {"dezenove", "nove", "noventa", "novecentos", "nove mil"} // 9
-            //   0         1         2          3             4
+            {"dez","zero", "", "", ""}, 
+            {"onze","um", "", "cento", "mil"},
+            {"doze", "dois", "vinte", "duzentos", "dois mil"}, 
+            {"treze", "três", "trinta", "trezentos", "três mil"},
+            {"quatorze", "quatro", "quarenta", "quatrocentos", "quatro mil"}, 
+            {"quinze", "cinco", "cinquenta", "quinhentos", "cinco mil"},
+            {"dezesseis", "seis", "sessenta", "seiscentos", "seis mil"},
+            {"dezesete", "sete", "setenta", "setecentos", "sete mil"},
+            {"dezoito", "oito", "oitenta", "oitocentos", "oito mil"},
+            {"dezenove", "nove", "noventa", "novecentos", "nove mil"} 
+            
         };
-		// Converto o 'a' ( que veio como String ), para inteiro. e passo ele como primeiro parametro da matriz 'unidade',
-		// o b já está em inteiro então só passo ele.
+        
         System.out.print(unidade[ Integer.parseInt((a)) ][b]); 
     }
     public static void um(){
@@ -44,53 +42,52 @@ public class Java3 {
         System.out.println("Digite um numero");
         String num = leitor.nextLine();
         leitor.close();
-        
-        // Definindo variaveis
+      
         String cen = "",
                 dez = "",
                 uni = "",
                 mii = ""; 
 
-        uni += num.charAt(num.length() - 1); // Pega o valor da Unidade
+        uni += num.charAt(num.length() - 1); 
 
-        if(num.length() > 3){ // Verifica se existem mais de 3 casas, ou seja, se é um milenio
-            mii += num.charAt( num.length() - 4 ); // Pega o valor do milenio
-            if(num.charAt( num.length() - 4 ) != '0'){ // Verifica se for zero, caso seje, não o escreve na tela
+        if(num.length() > 3){ 
+            mii += num.charAt( num.length() - 4 ); 
+            if(num.charAt( num.length() - 4 ) != '0'){ 
                 extenso(mii, 4);
                 System.out.print(" ");
-
+                
             }
         }
-        if(num.length() > 2){ // Verifica se existem mais de 2 casas, ou seja, se é uma centena
-            cen += num.charAt( num.length() - 3 ); // Pega o valor da centena
-            if(num.charAt( num.length() - 3 ) != '0'){ // Verifica se for zero, caso seje, não o escreve na tela
+        if(num.length() > 2){ 
+            cen += num.charAt( num.length() - 3 ); 
+            if(num.charAt( num.length() - 3 ) != '0'){ 
                 extenso(cen, 3);
                 if(num.charAt( num.length() - 2) != '0')
-                System.out.print(" e "); // Aqui ele digita o "e" dos numeros, é melhor deixar nessas horas pra não dar erro
-
+                System.out.print(" e ");
             }
         }
-        if(num.length() > 1){ // Verifica se a dezena existe
-            dez += num.charAt( num.length() - 2); // Pega o valor da dezena
-            if(num.charAt( num.length() - 2) != '0') { // Verifica se for zero, caso seje, não o escreve na tela
-                if(num.charAt( num.length() - 2) == '1') // Faz a casa dos 10, [onze,doze,treze...]
+        
+        if(num.length() > 1){
+            dez += num.charAt( num.length() - 2); 
+            if(num.charAt( num.length() - 2) != '0') {
+                if(num.charAt( num.length() - 2) == '1') 
                     extenso(uni, 0);
-                else{ // Senão... As demais dezenas
-    
+                else{ 
+                    
                     extenso(dez, 2);
                     if(num.charAt( num.length() - 1) != '0')
                     System.out.print(" e ");
                 }
             }
         }
-        try { // Este tenta executar mesmo existindo uma dezena
-            if(num.charAt( num.length() - 2) != '1') // Verifica se já existe 
+        
+        try { 
+            if(num.charAt( num.length() - 2) != '1')  
             extenso(uni, 1);
-        } catch (Exception e) { // se a dezena não existir ele executa o número normalmente 
+        } catch (Exception e) {
             extenso(uni, 1);
         }
     }
-    
     
     public static void dois(){
         Scanner leitor = new Scanner(System.in);
